b497fb73346b8e36cbdfd078b92efdf37e3d4f51
Vincenzo <urbisaglia@gmx.net>
2014-02-04 17:58:38 +0100

    jo mir händs geschafft

--- Vokabeltrainer/src/ch/Main.java
+++ Vokabeltrainer/src/ch/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	System.out.println("hollo vini");	
+	System.out.println("hollo chris");	
 		
 	}
 
