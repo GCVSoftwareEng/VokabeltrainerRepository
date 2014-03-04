e4ded79303e432b1b6be3ff85b3f45e890675488
Vincenzo <urbisaglia@gmx.net>
2014-03-04 20:23:42 +0100

    model update

--- Vokabeltrainer/src/ch/gcv/vokabeltrainer/model/ImportExportManager.java
+++ Vokabeltrainer/src/ch/gcv/vokabeltrainer/model/ImportExportManager.java
@@ -9,37 +9,46 @@ package ch.gcv.vokabeltrainer.model;
  * @author Vincenzo Urbisaglia
  * @version 1.0
  */
-public class ImportExportManager  implements IImportExportManager {
+public class ImportExportManager   {
 
+    private static ImportExportManager instance;
 
 	public ImportExportManager(){
 		super();
+		this.instance = null; // TODO
 		throw new UnsupportedOperationException("Not implemented");
 	}
  
-
     /** 
-     * exportTopic implements IImportExportManager.exportTopic
+     * exportTopic
 	 * @param topic // TODO 
 	 * @param path // TODO 
 	 *
 	 */
-	@Override
 	public void exportTopic(ITopic topic,String path) {
 		// TODO should be implemented
 		throw new UnsupportedOperationException("Not implemented");
     }
 
     /** 
-     * importTopic implements IImportExportManager.importTopic
+     * importTopic
 	 * @param path // TODO 
 	 * @return boolean  // TODO
 	 */
-	@Override
 	public boolean importTopic(String path) {
 		// TODO should be implemented
 		throw new UnsupportedOperationException("Not implemented");
     }
+
+    /** 
+     * getInstance
+	 * @return ImportExportManager  // TODO
+	 */
+	public static ImportExportManager getInstance() {
+		// TODO should be implemented
+		throw new UnsupportedOperationException("Not implemented");
+    }
+
 
 }
  
